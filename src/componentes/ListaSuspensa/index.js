917256ac25d8b2079eca99fb1389d129bbc1b603
Thiagouvea <thiagouvea90@gmail.com>
2023-09-25 08:20:50 -0300

    validando formulario

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,10 +1,13 @@
 import "./ListaSuspensa.css";
 
 const ListaSuspensa = (props) => {
+    console.log(props.itens)
     return (
-        <div>
+        <div className="lista-suspensa"> 
             <label>{props.label}</label>
-            <select></select>
+            <select>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
         </div>
     )
 }
